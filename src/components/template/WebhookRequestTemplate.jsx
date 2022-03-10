cfac7a50bf7cb9e35d3cbcaeb78914e954be1e72
hridoy-orangetoolz <hridoy@orangetoolz.com>
2022-03-11 00:17:16 +0600

    feat: url and json validation

--- src/components/template/WebhookRequestTemplate.jsx
+++ src/components/template/WebhookRequestTemplate.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import "./requestTemplate.css";
 import { WEBHOOK_REQUESTS } from "../../constants/TemplateConstants";
 import CustomSelect from "../custom/CustomSelect";
 
 const WebhookRequestTemplate = () => {
+  const [data, setData] = useState(null);
+  const [endpoint, setEndpoint] = useState("");
+
+  const isValidJSONString = (str) => {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+  }
+
+  const isValidHttpUrl =(str)=>{
+    let pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }
+
+  const handleValidEndpoint = e => {
+    if(isValidHttpUrl(e.target.value)) {
+      setEndpoint(e.target.value);
+    }
+  }
+
+  const handleChangeDataLayer = (e) => {
+    if(isValidJSONString(e.target.value)){
+      setData(e.target.value);
+    }
+  }
+  console.log("Data", data);
+  console.log("Endpoint", endpoint);
   return (
     <>
       <div className="webhook__request__each__section">
@@ -30,15 +65,16 @@ const WebhookRequestTemplate = () => {
 
       <div className="webhook__request__each__section">
         <span>Request Endpoint</span>
-        <input type="text" className="webhook__request__endpoint" />
+        <input type="text" className="webhook__request__endpoint" onChange={handleValidEndpoint} />
       </div>
 
       <div className="webhook__request__each__section">
-        <span>Authentication Layer</span>
+        <span>Authentication</span>
       </div>
 
       <div className="webhook__request__each__section">
-        <span>Data Layer</span>
+        <span>Data</span>
+        ​<textarea id="txtArea" rows="10" cols="70" onChange={handleChangeDataLayer}></textarea>
       </div>
     </>
   );
